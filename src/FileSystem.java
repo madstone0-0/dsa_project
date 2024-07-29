8e35841bb451acf7f1270ac63221353c25298fef
Ama-Annor <136464888+Ama-Annor@users.noreply.github.com>
2024-07-29 13:24:44 +0000

    Update FileSystem.java

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -1,7 +1,38 @@
-public interface FileSystem {
-  String getName();
-  void rename(String newName);
-  FileSystem getParent();
-  void setParent(FileSystem parent);
-  boolean isDirectory();
+import java.time.LocalDateTime;
+
+public abstract class FileSystem {
+    protected String name;
+    protected long size;
+    protected LocalDateTime dateCreated;
+    protected LocalDateTime dateModified;
+
+    public FileSystem(String name) {
+        this.name = name;
+        this.dateCreated = LocalDateTime.now();
+        this.dateModified = LocalDateTime.now();
+        this.size = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void rename(String newName) {
+        this.name = newName;
+        this.dateModified = LocalDateTime.now();
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public LocalDateTime getDateModified() {
+        return dateModified;
+    }
+
+    public abstract boolean isDirectory();
 }
