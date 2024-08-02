4bb4cbfc6b85368c00022153b9d9837239e3a46f
Ama-Annor <136464888+Ama-Annor@users.noreply.github.com>
2024-08-02 15:26:43 +0000

    Update Directory.java

--- src/Directory.java
+++ src/Directory.java
@@ -2,7 +2,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Directory extends FileSystem {
+public class Directory extends FileSystem { 
 
     public Directory(String name) {
         super(name);
