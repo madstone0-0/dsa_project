ceb62e722b034fa6818c28675f1b046691da695a
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-08-03 13:52:57 +0000

    Changed test tree

--- src/Main.java
+++ src/Main.java
@@ -21,30 +21,49 @@ import static utils.PrintUtils.println;
  */
 public class Main {
     private static void testTree(DirectoryTree tree) {
-        var rand = new Random();
-        for (int i = 0; i < 5; i++) {
-            var node = tree.create(new Directory("dir" + i));
-            tree.cd(node);
-            for (int j = 0; j < 5; j++) {
-                try {
-                    tree.create(new File("file" + rand.nextInt(10), "txt", 100));
-                } catch (IllegalArgumentException e) {
-                }
-            }
-            tree.cd((GeneralTreeNode<FileSystem>) tree.getDirectoryTree().root());
-        }
+        // Create the src directory and its substructure
+        GeneralTreeNode<FileSystem> srcNode = tree.create(new Directory("src"));
+        tree.cd(srcNode);
 
-        for (int i = 0; i < 5; i++) {
-            try {
-                tree.create(new File("file" + i, "txt", 100));
-            } catch (IllegalArgumentException e) {
-            }
-        }
+        String[] srcFiles = {"AbBinaryTree", "AbGeneralTree", "AbTree", "BinarySearchTree", "BinaryTreeADT",
+                "BinaryTreeNode", "CommandMode", "Common", "Directory", "DirectoryTree", "File", "FileSystem",
+                "GeneralTreeNode.java", "LinkedGeneralTree", "Main", "MenuMode", "TreeADT", "TreeNode"};
+        GeneralTreeNode<FileSystem> utilsNode = tree.create(new Directory("utils"));
+        tree.cd(utilsNode);
+        tree.create(new File("PrintUtils", "java", 100));
+        tree.cd((GeneralTreeNode<FileSystem>) tree.getWd().parent);
 
+        for (String fileName : srcFiles) {
+            tree.create(new File(fileName, "java", 100));
+        }
         tree.cd((GeneralTreeNode<FileSystem>) tree.getDirectoryTree().root());
-        tree.cd(tree.create(new Directory("paste")));
-        tree.create(new File("test", "txt", 100));
+
+        // Create the out directory and its substructure
+        GeneralTreeNode<FileSystem> outNode = tree.create(new Directory("out"));
+        tree.cd(outNode);
+        GeneralTreeNode<FileSystem> productionNode = tree.create(new Directory("production"));
+        tree.cd(productionNode);
+        GeneralTreeNode<FileSystem> dsaProjectNode = tree.create(new Directory("dsa_project"));
+        tree.cd(dsaProjectNode);
+
+        String[] outFiles = {"AbBinaryTree", "AbGeneralTree", "AbTree", "BinarySearchTree", "BinaryTreeADT",
+                "BinaryTreeNode", "CommandMode", "Common", "Directory", "DirectoryTree", "File", "FileSystem",
+                "GeneralTreeNode", "LinkedGeneralTree", "Main", "MenuMode$ItemType", "MenuMode", "TreeADT", "TreeNode"};
+        utilsNode = tree.create(new Directory("utils"));
+        tree.cd(utilsNode);
+        tree.create(new File("PrintUtils", "class", 100));
+        tree.cd((GeneralTreeNode<FileSystem>) tree.getWd().parent);
+
+        for (String fileName : outFiles) {
+            tree.create(new File(fileName, "class", 100));
+        }
         tree.cd((GeneralTreeNode<FileSystem>) tree.getDirectoryTree().root());
+
+        // Create the root-level files
+        String[] rootFiles = {"dsa_project"};
+        for (String fileName : rootFiles) {
+            tree.create(new File(fileName, "iml", 100));
+        }
     }
 
 
@@ -66,7 +85,7 @@ public class Main {
         }
 
         DirectoryTree directoryTree = new DirectoryTree(new Directory(""));
-//        testTree(directoryTree);
+        testTree(directoryTree);
 
         try (Scanner in = new Scanner(System.in)) {
             if (mode) {
